ceb454f9c2cba17046a0c61ec3f3851191649291
DevTamakuwala <dev.official2424@gmail.com>
2024-11-19 23:32:54 +0530

    Working on API.

--- src/main/java/com/businesssehyogi/BusinessSehyogi/Controller/InvestedController.java
+++ src/main/java/com/businesssehyogi/BusinessSehyogi/Controller/InvestedController.java
@@ -23,4 +23,15 @@ public class InvestedController {
     public Invested getInvestment(@PathVariable("investmentId") int investmentId) {
         return investedRepository.findByInvestId(investmentId);
     }
+
+    @DeleteMapping("/deleteInvestment/{investmentId}")
+    public String deleteInvestment(@PathVariable("investmentId") int investmentId) {
+        Invested invested = investedRepository.findByInvestId(investmentId);
+        if (investedRepository.existsById(investmentId)) {
+            investedRepository.deleteById(investmentId);
+            return "Investment Deleted successfully";
+        } else {
+            return "No investment found";
+        }
+    }
 }
